d99421495c9381b0201962355d6dfb7fccec6150
Emma Meehan <emma.meehan3@hotmail.com>
2022-07-18 12:02:27 +0100

    First commit

---
+++ src/main/java/com/precisionsportstech/movementassessment/MovementAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.precisionsportstech.movementassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/precisionsportstech/movementassessment/MovementAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precisionsportstech.movementassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
